8c9ff2d6b248d38063d6792ddaac752f379e3aba
Ollie Watkins <nmd12kzu@uea.ac.uk>
2024-09-12 14:46:46 +0100

    minor tidy
    removing uncomments

--- app/components/track/track.tsx
+++ app/components/track/track.tsx
@@ -56,33 +56,3 @@ const Track = ({ id, title, artist, genre, length, deleteTrack, editTrack }: Tra
 };
 
 export default Track;
-
-// <li data-component={"track " + id} className="flex py-2 list-none items-center justify-between">
-//   <>
-//     {/* edit, song title and artist */}
-//     <div className="flex items-center w-3/5">
-//       <button value={id} onClick={(e) => handleEditTrack(e)} className="mr-4">
-//         <FaEdit className="pointer-events-none" />
-//       </button>
-//       <div className="flex flex-col">
-//         <span className="font-bold">{title}</span>
-//         {artist && <span className="mx-1 max-w-40 text-ellipsis overflow-hidden">by {artist}</span>}
-//       </div>
-//     </div>
-//   </>
-//   <>
-//     {/* genre */}
-//     <div className="flex justify-between mx-12">
-//       <span className="">{genre}</span>
-//     </div>
-//   </>
-//   <>
-//     {/* song length and delete */}
-//     <div className="flex justify-between w-16">
-//       <span className="">{length}</span>
-//       <button value={id} onClick={(e) => handleDeleteTrack(e)} className="ml-auto">
-//         <FaRegTrashAlt className="pointer-events-none" />
-//       </button>
-//     </div>
-//   </>
-// </li>
